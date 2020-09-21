7ae8c550bc0374a3b4b23c87109c249ae80a1dfc
FabAhl <fabian.ahlqvist@gmail.com>
2020-09-21 21:34:04 +0200

    Fixat i CSS

--- script/validering.js
+++ script/validering.js
@@ -565,3 +565,19 @@ addWarningToCommentAreaWhenLoading("#textComment")
 
 
 checkReadyToSendComment()
+
+
+$(function(){
+
+var text1 = $("#headerForMain");
+text1.fadeIn(2750);
+
+var text2 = $("#textForMain");
+text2.fadeIn(4550);
+
+})
+
+
+
+
+
