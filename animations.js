9fea4fc077b256f92655d83ef3dcd6384b1bd61e
FabAhl <fabian.ahlqvist@gmail.com>
2020-09-24 01:33:08 +0200

    La till animationer i Kontakt

--- animations.js
+++ animations.js
@@ -6,4 +6,11 @@ $(function(){
     var text2 = $("#textForMain");
     text2.fadeIn(4550);
     
+    var text3 = $("#subHeadingContact");
+    text3.fadeIn(2750);
+    
+    var text4 = $("#subHeadingContact2");
+    text4.fadeIn(4550);
+
+
     })
