1b83c56d46b413274c83293e93bbf4c6f891e4bb
FabAhl <fabian.ahlqvist@gmail.com>
2020-09-27 18:33:52 +0200

    Refaktoriserat ändrat så det framgår att det är jquery variabler samt fixat så det ser finare ut

--- animations.js
+++ animations.js
@@ -1,121 +1,99 @@
-
- 
-
-
-
-$(function(){
+$(function () {
   var size = $(window).width();
-  $(window).on('resize', function(){
-    var size = $(window).width();
-  
-  console.log("Storlek" + size);
+  $(window).on('resize', function () {
+    var $screenSize = $(window).width();
+
+    console.log("Storlek" + size);
   });
 
-    var text1 = $("#headerForMain");
-    text1.fadeIn(2750);
-    
-    var text2 = $("#textForMain");
-    text2.fadeIn(4550);
-    
-    var text3 = $("#subHeadingContact");
-    text3.fadeIn(2750);
-    
-    var text4 = $("#subHeadingContact2");
-    text4.fadeIn(4550);
-
-    
-    function mouseHoverOverProject(projectId){
-      
-      $(projectId).mouseenter(function() {
-       
-       var $projectDiv= $(projectId);
-       $projectDiv.css("background-image", "none");
-       $projectDiv.css("background-color", "teal");
-       $projectDiv.children().removeClass("notVisible");
-       $projectDiv.children().addClass("visible");
-    
-
-      
-     
-  });
-    }
-   
-    function mouseLeavingProject(projectId, backgroundImage){
-      
-      
-      $(projectId).mouseleave(function() {
-       
-         console.log("MouseLeave"); 
-        var $projectDiv= $(projectId);
-        $projectDiv.css("background-image", backgroundImage);
-        $projectDiv.css("background-color", "teal");
-        $projectDiv.children().removeClass("visible");
-        $projectDiv.children().addClass("notVisible");
-  
-      
-        
+  function mouseHoverOverProject(projectId) {
+    $(projectId).mouseenter(function () {
+      var $projectDiv = $(projectId);
+      $projectDiv.css("background-image", "none");
+      $projectDiv.css("background-color", "teal");
+      $projectDiv.children().removeClass("notVisible");
+      $projectDiv.children().addClass("visible");
     });
-    }
+  }
 
-    
+  function mouseLeavingProject(projectId, backgroundImage) {
+    $(projectId).mouseleave(function () {
+      console.log("MouseLeave");
+      var $projectDiv = $(projectId);
+      $projectDiv.css("background-image", backgroundImage);
+      $projectDiv.css("background-color", "teal");
+      $projectDiv.children().removeClass("visible");
+      $projectDiv.children().addClass("notVisible");
+    });
+  }
 
+  var $animatedHeaderForMain = $("#headerForMain");
+  $animatedHeaderForMain.fadeIn(2750);
 
-   
+  var $animatedTextForMain = $("#textForMain");
+  $animatedTextForMain.fadeIn(4550);
 
-      var text5 = $(".barMeterQuarter");
-      text5.animate({width: "+=25%"}, 3000);
-      
-      var text6 = $(".barMeterHalf");
-      text6.animate({width: "+=50%"}, 3000);
+  var $animatedSubHeadingContact1 = $("#subHeadingContact");
+  $animatedSubHeadingContact1.fadeIn(2750);
 
-      var text7 = $(".barMeterFull");
-      text7.animate({width: "+=95%"},3000);
+  var $animatedSubHeadingContact2 = $("#subHeadingContact2");
+  $animatedSubHeadingContact2.fadeIn(4550);
 
+  var $animatedBarMeterQuarter = $(".barMeterQuarter");
+  $animatedBarMeterQuarter.animate({ width: "+=25%" }, 3000);
 
-      var text8 = $("#omOssHeading");
-      text8.animate({
-        "marginLeft": "+=20%"}, 1000);
+  var $animatedBarMeterHalf = $(".barMeterHalf");
+  $animatedBarMeterHalf.animate({ width: "+=50%" }, 3000);
 
-      var text9 = $("#headertextForPerson");
-      text9.animate({
-        "marginLeft": "+=13%"}, 1000);  
-        
-      var text10 = $(".centerOmOss");
-      text10.animate({
-          "marginTop": "+=-15%"}, 1000);  
+  var $animatedBarMeterFull = $(".barMeterFull");
+  $animatedBarMeterFull.animate({ width: "+=95%" }, 3000);
 
-          var text11 = $("#headertextForKontakt");
-      text11.animate({
-        "marginLeft": "+=16%"}, 1000);
+  var $animatedOmOssHeading = $("#omOssHeading");
+  $animatedOmOssHeading.animate({
+    "marginLeft": "+=20%"
+  }, 1000);
 
-        var text12 = $("#mainOmOssContact");
-        text12.animate({
-          "marginTop": "+=-4%"}, 1000);
-     
-      mouseLeavingProject("#sortingVisualizer", "url(sortingvisualizer.PNG)")
-      mouseHoverOverProject("#sortingVisualizer")
-      mouseLeavingProject("#interActiveDesign", "url(cykelApp.jpg")
-      mouseHoverOverProject("#interActiveDesign")
-      
-      mouseLeavingProject("#sparKoll", "url(sparKollen.PNG)")
-      
-      mouseHoverOverProject("#sparKoll")
-      
-      mouseLeavingProject("#dataVisualizer", "url(dataVi.png)")
-      
-      mouseHoverOverProject("#dataVisualizer")
-      
-      mouseLeavingProject("#collision", "url(88412527-06457000-cdda-11ea-9f01-a69bc80b391c.png)")
-      
-      mouseHoverOverProject("#collision")
-      
-      mouseLeavingProject("#inDev", "url(inDevImg.jpg)")
-      
-      mouseHoverOverProject("#inDev", "#sortingVisualizer")
-     
-  
+  var $animatedHeaderTextForPerson = $("#headertextForPerson");
+  $animatedHeaderTextForPerson.animate({
+    "marginLeft": "+=13%"
+  }, 1000);
 
-       
-    })
+  var $animatedCenterOmOss = $(".centerOmOss");
+  $animatedCenterOmOss.animate({
+    "marginTop": "+=-15%"
+  }, 1000);
 
-      
+  var $headerTextForKontakt = $("#headertextForKontakt");
+  $headerTextForKontakt.animate({
+    "marginLeft": "+=16%"
+  }, 1000);
+
+  var $mainOmOssContact = $("#mainOmOssContact");
+  $mainOmOssContact.animate({
+    "marginTop": "+=-4%"
+  }, 1000);
+
+  mouseLeavingProject("#sortingVisualizer", "url(sortingvisualizer.PNG)");
+
+  mouseHoverOverProject("#sortingVisualizer");
+
+  mouseLeavingProject("#interActiveDesign", "url(cykelApp.jpg");
+
+  mouseHoverOverProject("#interActiveDesign");
+
+  mouseLeavingProject("#sparKoll", "url(sparKollen.PNG)");
+
+  mouseHoverOverProject("#sparKoll");
+
+  mouseLeavingProject("#dataVisualizer", "url(dataVi.png)");
+
+  mouseHoverOverProject("#dataVisualizer");
+
+  mouseLeavingProject("#collision", "url(88412527-06457000-cdda-11ea-9f01-a69bc80b391c.png)");
+
+  mouseHoverOverProject("#collision");
+
+  mouseLeavingProject("#inDev", "url(inDevImg.jpg)");
+
+  mouseHoverOverProject("#inDev", "#sortingVisualizer");
+})
